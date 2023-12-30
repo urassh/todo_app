dfeb45218312576bbef13eed2eec1f60341efa65
urassh <shuto.1011@icloud.com>
2023-12-30 12:25:04 +0900

    feat: listPageにもimportantを表示できるようにした

--- lib/DataModel/Todo.dart
+++ lib/DataModel/Todo.dart
@@ -1,6 +1,12 @@
+import 'package:flutter/material.dart';
+
 class Todo {
   final String name;
   final bool isImportant;
 
   const Todo({required this.name, this.isImportant = false});
+
+  MaterialColor importantColor() {
+    return isImportant ? Colors.yellow : Colors.grey;
+  }
 }
